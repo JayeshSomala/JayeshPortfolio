917ba9330b62fbb147af459d41a9e1b5ba51d0e6
JayeshSomala <jayeshsomala@gmail.com>
2024-10-29 10:05:00 +0530

    added responsivess

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {MainPage} from './components/MainPage';
 import {JayeshPortfolio} from './components/JayeshPortfolio';
-import {PranjalPortfolio} from './components/PranjalPortfolio';
 
 function App() {
   return (
